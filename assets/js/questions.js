a36f00352a433e5f1e389b9ed6f06d99be297412
ibalan92 <ibalan92@gmail.com>
2023-11-13 20:22:49 +0000

    Created 3 questions for testing

---
+++ assets/js/questions.js
@@ -0,0 +1,11 @@
+var quizQuestion1 = {
+    question: "Inside which HTML element do we put the JavaScript?",
+    choices: ["<javascript>", "<js>", "<script>", "<scripting>"],
+    answer: "<script>"
+}
+
+var quizQuestion2 = {
+    question: "Where is the correct place to insert a JavaScript?",
+    choices: ["The <head> section", "Both the <head> and the <body> section are correct", "The <body> section", "Anywhere"],
+    answer: "Both the <head> and the <body> section are correct"
+}
